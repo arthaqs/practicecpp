ed9f4aad1c0068937857f75ab8a9c31b70284ec7
wheresthecat <frosty@centrum.cz>
2022-02-01 18:12:48 +0100

    Create one.cpp

---
+++ w3resource/one.cpp
@@ -0,0 +1,12 @@
+// Basic stuff.
+// Exercise number one.
+// Write a program in Cpp to print a welcome text in a seperate line.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome text in a separate line." << std::endl;
+    std::cout << "The line." << std::endl;
+
+    return(0);
+}
